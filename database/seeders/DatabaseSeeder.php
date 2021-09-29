1efe0ad687b378ba5ec9f6ab5a6829ef54180f87
pkhirapara <pkhirapara@gmail.com>
2021-09-29 23:06:07 +0200

    seeder file changed, customer blade file changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Customer::factory(100)->create();
+        Customer::factory(10)->create();
     }
 }
--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -9,31 +9,33 @@
     <title>Craftboxx</title>
   </head>
   <body>
-    <h1>Craftboxx Customers</h1>
 
-    <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Commercial</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($customer as $customer_deatils)
-          <tr>
-            <th scope="row">{{ $loop->index + 1 }}</th>
-            <td>{{ $customer_deatils->firstname }}</td>
-            <td>{{ $customer_deatils->lastname }}</td>
-            <td>{{ $customer_deatils->isCommercial?"Yes":"No" }}</td>
-          </tr>
-          @endforeach
+      <div class="container">
 
-          
-        </tbody>
-      </table>
+        <table class="table table-striped table-hover caption-top">
+            <caption>List of Customers</caption>
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Commercial</th>
+              </tr>
+            </thead>
 
+            <tbody>
+                @foreach ($customer as $customer_deatils)
+              <tr>
+                <th scope="row">{{ $loop->index + 1 }}</th>
+                <td>{{ $customer_deatils->firstname }}</td>
+                <td>{{ $customer_deatils->lastname }}</td>
+                <td>{{ $customer_deatils->isCommercial?"Yes":"No" }}</td>
+              </tr>
+              @endforeach         
+            </tbody>
+        </table>
+
+      </div>
 
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
 
